7f2be96c842e507c2119e860d6f6749832ea4e0a
pedro-santos28 <phdss04@gmail.com>
2023-10-02 13:39:59 -0300

    feat: showing the i/o icon

--- src/components/Process/components/CpuComponent.tsx
+++ src/components/Process/components/CpuComponent.tsx
@@ -1,5 +1,6 @@
 import { useProcessesContext } from '@/context/context';
-import { Cpu } from '@phosphor-icons/react'
+import { ProcessType } from '@/enums';
+import { Cpu, Keyboard } from '@phosphor-icons/react'
 import React from 'react'
 
 type Props = {}
@@ -12,23 +13,29 @@ export default function CpuComponent({}: Props) {
 
   return (
     <div className="flex items-center flex-col gap-2 p-4 shadow-lg rounded-lg h-[200px] w-full">
-    <Cpu
-      size={40}
-      className={`${activeProcess ? 'animate-ping text-red-500' : ''}`}
-    />
+
+      {activeProcess?.type === ProcessType.CpuBound ? (
+        <Cpu
+          size={50}
+        />
+      ) :  (
+        <Keyboard 
+          size={50}
+        />
+      )}
 
     {activeProcess && (
       <div className="flex flex-col p-4 items-center">
         <div className="flex p-2 gap-2 items-center">
-          <h2 className="text-green-500 italic">Executando...</h2>
+          <h2 className="text-green-500 italic text-lg font-semibold">Executando...</h2>
         </div>
 
         <div className="flex gap-1 items-center">
           <span
             style={{ backgroundColor: activeProcess?.color }}
-            className={`rounded-full h-2 w-2 inline-block`}
+            className={`rounded-full h-2 w-2 inline-block animate-ping`}
           />
-          <span>{activeProcess?.id}</span>
+          <span className='text-lg font-semibold'>{activeProcess?.id}</span>
         </div>
       </div>
     )}
